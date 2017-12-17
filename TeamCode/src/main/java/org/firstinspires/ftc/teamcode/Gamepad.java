d588a9beebc7dabe75da94d0d6a9fa5a9c4fd0ab
flavmodan <flavmodan@gmail.com>
2017-12-17 02:35:03 +0200

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
@@ -50,6 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+
 @TeleOp(name="Gamepad", group="Linear Opmode")
 
 public class Gamepad extends LinearOpMode {
