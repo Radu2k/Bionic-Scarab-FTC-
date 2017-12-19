8de2f859aafddf3bd0e5e2c53880af8db50e19b1
radusalavat <radusalavat48@gmail.com>
2017-12-19 22:13:58 +0200

    Ultimul

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls.java
@@ -58,8 +58,8 @@ public class controls {
     public void grab(){
         if(timegrab.seconds()>1)
         if(grab_cub_check==true ) {
-            grab_cube_right.setPosition(grab_cub_poz-0.5 );
-            grab_cube_left.setPosition(grab_cub_poz+0.7);
+            grab_cube_right.setPosition(grab_cub_poz-0.8 );
+            grab_cube_left.setPosition(grab_cub_poz+0.2);
             grab_cub_check=false;
             timegrab.reset();
             timegrab.startTime();
@@ -67,8 +67,8 @@ public class controls {
         }
                 else {
 
-            grab_cube_right.setPosition(grab_cub_poz + 0.5);
-            grab_cube_left.setPosition(grab_cub_poz - 0.7);
+            grab_cube_right.setPosition(grab_cub_poz + 0.8);
+            grab_cube_left.setPosition(grab_cub_poz - 0.2);
             grab_cub_check = true;
             timegrab.reset();
             timegrab.startTime();
